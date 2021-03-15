e3ddfb04d09d0556d5e2916915c428558a7c3e50
Alina <alinakazakbaeva@yandex.ru>
2021-03-15 10:42:31 +0300

    desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import "./import/modules";
 
+let tabBtn = document.querySelectorAll('.tabs__item');
+let tabBody = document.querySelectorAll('.tab-body');
+tabBtn.forEach(function(item){
+
+  item.onclick = function(){
+    let tabId = item.getAttribute('data-tab');
+    let currentId = document.querySelector(tabId);
+    
+    tabBtn.forEach(function(item){
+      item.classList.remove('active');
+    });
+    tabBody.forEach(function(item){
+      item.classList.remove('active');
+    });
+    item.classList.add('active');
+    currentId.classList.add('active');
+  }
+});
